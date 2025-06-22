d5084e4fe76fd3a9c6aca19b0aff0c58df336d5e
Andy Le <4ndyle@gmail.com>
2025-06-22 11:21:15 -0700

    Solve problem using 2 stacks

--- Unit3/advanced.py
+++ Unit3/advanced.py
@@ -1,73 +1,40 @@
-# hello wworld
+"""
+Understand:  
+  Input: list (strings: schedule, cancel, or reschedule)
+  Output: List (ID x on the stage)
+  Constraints: 
+  Edge CaseS: 
+    - Empty list: return []
 
-def is_valid_post_format(posts):
-    stack = []
-    hmap = {")":"(", "]":"[","}":"{"}
+  Plan: 
+  Initialize 2 stacks (one represents the scheudle and one represents canceled)
 
-    for char in posts:
-        if char not in hmap:
-            stack.append(char) 
-        else:
-            if stack:
-                n = stack.pop()
-                if not hmap[char] == n:
-                    return False 
-    return True if len(stack) == 0 else False
-                
-# U ...check if the parens are valid, thus opened is followed with close for a particular
-#      type of parenthesis
-#   i - string of parens    o - boolean    c -- open must match with close  e- empty string True
-# P ..
-#    High level: use stacks for this problem
-#                use dict to track the correct pair for a particular parenthesis {):(}
-#                if we meet an open parens we add to the stack
-#                if we meet a close parens, if the stack is not empty : we pop from the stack and check
-#                if key ( current close parens) h as
-#                correct open parens as the value
-#                if the stack is empty we return True, otherwise False
-#                ()))
-# Implementation
+  for string in changes: 
+    if it is schedule:
+      add to the schedule stack
+    otherwise, if it cancel:
+      pop off the scheudle stack, add to canceled stack
+    otherwise, rechsdule:
+      pop off the cancled stack, add back to schedule 
 
+  return schedule stack
+"""
 
-# print(is_valid_post_format("()"))
-# print(is_valid_post_format("()[]{}")) 
-# print(is_valid_post_format("(]"))
+def manage_stage_changes(changes):
+  schedule = []
+  canceled = []
 
-'''
-On your platform, comments on posts are displayed in the order they are received. However, for a special feature, you need to reverse the order of comments before displaying them. Given a queue of comments represented as a list of strings, reverse the order using a stack.
+  for change in changes:
+    if "Schedule" in change:
+      schedule.append(change[-1])
+    elif change == "Cancel":
+      canceled.append(schedule.pop())
+    else:
+      schedule.append(canceled.pop())
 
-Understand: 
-  Input: queue (represents comments as a list of strings)
-  Output: list (comments in reversed order)
-  Constraints / Considerations: Use a stack
-  Edge Cases / Examples: 
-    - Empty Queue: return empty list
+  return schedule
 
-Plan: 
-  Initialize an empty list (for results) and an empty stack (store the comments to reverse)
-
-  For comment in comments:
-    append comment to the stack
-
-  pop off each comment on the stack while adding each comment to the result list
-
-  return the list of results
-'''
-
-def reverse_comments_queue(comments):
-  stack = []
-  results = []
-
-  for comment in comments:
-      stack.append(comment)
-
-  while stack:
-      results.append(stack.pop())
-
-  return results
-
-# print(reverse_comments_queue(["Great post!", "Love it!", "Thanks for sharing."]))
-# print(reverse_comments_queue(["First!", "Interesting read.", "Well written."]))
-
-
+# print(manage_stage_changes(["Schedule A", "Schedule B", "Cancel", "Schedule C", "Reschedule", "Schedule D"]))  
+# print(manage_stage_changes(["Schedule A", "Cancel", "Schedule B", "Cancel", "Reschedule", "Cancel"])) 
+# print(manage_stage_changes(["Schedule X", "Schedule Y", "Cancel", "Cancel", "Schedule Z"])) 
 
