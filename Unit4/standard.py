7648697212f4c1399ebcb49b48d8053ef379cc08
Andy Le <4ndyle@gmail.com>
2025-06-25 20:09:02 -0700

    Answer question related to time and space complexity

---
+++ Unit4/standard.py
@@ -0,0 +1,48 @@
+"""
+You're curating a large collection of NFTs for a digital art gallery, and your first task is to extract the names of these NFTs from a given list of dictionaries. Each dictionary in the list represents an NFT, and contains information such as the name, creator, and current value.
+
+Write the extract_nft_names() function, which takes in this list and returns a list of all NFT names.
+
+Evaluate the time and space complexity of your solution. Define your variables and provide a rationale for why you believe your solution has the stated time and space complexity.
+
+Input: list (of dictionaries representing NFT's and their information)
+Output: list (containing names of each NFT)
+Constraints: 
+Edge Cases:
+  - Empty list: return empty list 
+
+Plan: 
+Initliaze empty list to store results
+
+for nft in nft_colletion: 
+  add name of each nft to results
+
+return results
+"""
+
+def extract_nft_names(nft_collection):
+  nft_names = []
+
+  for nft in nft_collection:
+    nft_names.append(nft["name"])
+
+  return nft_names
+
+nft_collection = [
+    {"name": "Abstract Horizon", "creator": "ArtByAlex", "value": 5.4},
+    {"name": "Pixel Dreams", "creator": "DreamyPixel", "value": 7.2},
+    {"name": "Future City", "creator": "UrbanArt", "value": 3.8}
+]
+
+nft_collection_2 = [
+    {"name": "Crypto Kitty", "creator": "CryptoPets", "value": 10.5},
+    {"name": "Galactic Voyage", "creator": "SpaceArt", "value": 6.7}
+]
+
+nft_collection_3 = [
+    {"name": "Golden Hour", "creator": "SunsetArtist", "value": 8.9}
+]
+
+print(extract_nft_names(nft_collection))
+print(extract_nft_names(nft_collection_2))
+print(extract_nft_names(nft_collection_3))
