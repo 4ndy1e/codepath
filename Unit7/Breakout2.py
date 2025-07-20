a3d1f7af9324ea736310ca7d17111a7cf83ece3c
Andy Le <4ndyle@gmail.com>
2025-07-20 11:57:22 -0700

    Solve breakout room problems unit 7 session 2

---
+++ Unit7/Breakout2.py
@@ -0,0 +1,139 @@
+import math
+
+"""
+U
+    I - integer list and integer ID
+    O - boolean
+    C - solution must have O(log n) time complexity
+      - integer list is sorted in ascending order
+    E - [] -> False
+      - no integer ID in the list -> False
+
+P
+    HL: Return False if inventory == []. Initialize left = 0
+    and right = len(inventory - 1). Compute mid = (left + right) // 2
+    While left <= right, if inventory[mid] == part_id, return True. If
+    midpoint < part_id, update left to mid + 1. Otherwise, update right to 
+    mid - 1. Rdturn False at the end.
+
+Time: O(log n)
+Space: O(1)
+"""
+def check_stock(inventory, part_id):
+    if inventory == []:
+        return False
+
+    left, right = 0, len(inventory) - 1
+    
+    while left <= right:
+        mid = (left + right) // 2
+        if inventory[mid] == part_id:
+            return True
+        elif inventory[mid] < part_id:
+            left = mid + 1
+        else:
+            right = mid - 1
+    
+    return False
+
+# print(check_stock([1, 2, 5, 12, 20], 20))
+# print(check_stock([1, 2, 5, 12, 20], 100))
+
+
+"""
+Problem 2
+input: 
+    inventory: list of inventory id's 
+    part_id: id of the part that we are looking for 
+output:
+    boolean: whether or not the part exists in the inventory
+constraints: 
+    - inventory ids are sprted om ascending order 
+    - log n time complexity 
+    - use recursion
+edge cases:
+    - empty list -> return false 
+    - not found -> return false 
+    
+Plan: Use binary search recursion to search through the inventory, partitioning the list in half whenever the 
+inventory id is not found. Search the left or right partition depending on the id's value (whether it is greater than or less than the middle value).
+
+Time and Space Complexity:
+Time: O(log n)
+Space: O(log n)
+"""
+def check_stock_recursively(inventory, part_id):
+    if not inventory: 
+        return False
+    
+    def search_stock(left, right, target):
+        # base case 
+        if left > right:
+            return False
+        
+        mid = (left + right) // 2
+        
+        if inventory[mid] == target:
+            return True
+        elif inventory[mid] > target: 
+            return search_stock(left, mid-1, target)
+        return search_stock(mid+1, right, target)
+    
+    return search_stock(0, len(inventory) - 1, part_id)
+
+# print(check_stock_recursively([1, 2, 5, 20, 12], 20))
+# print(check_stock_recursively([1, 2, 5, 20, 12], 100))
+
+"""
+U
+    I - integer array and integer target
+    O - integer tuple (with the first and last indices for the target)
+    C - solution must have O(log n) time complexity
+      - integers in array are sorted
+    E - [] -> (-1, -1)
+      - the target is not in array -> (-1, -1)
+
+P
+    HL: Return (-1, -1) if transmissions == []. Initialize left = 0 and right = len(transmissions) - 1.
+    Initialize first_occurrence and last_occurrence to math.inf. 
+    Compute mid = (left + right) // 2. While left <= right, if midpoint == target, first_occurrence will be equal to mid, and break
+    from the loop. If midpoint is greater, update right to mid - 1. else, update left to mid + 1. if first_occurrence > len(transmissions),
+    return (-1, -1). For i in range(first_occurrence, len(transmissions)), if i is equal == target, update last_occurrence.
+    Return (first, last).
+"""
+def find_frequency_positions(transmissions, target_code):
+    if not transmissions:
+        return (-1, -1)
+
+    left, right = 0, len(transmissions) - 1
+    first_occurrence = last_occurrence = math.inf
+
+    while left <= right:
+        mid = (left + right) // 2
+        if transmissions[mid] == target_code:
+            first_occurrence = mid
+            break
+        elif transmissions[mid] > target_code:
+            right = mid - 1
+        else:
+            left = mid + 1
+    
+    if first_occurrence > len(transmissions):
+        return (-1, -1)
+
+    i = first_occurrence
+    while transmissions[i] == target_code:
+        first_occurrence = i
+        i -= 1
+
+    last_occurrence = first_occurrence
+    for i in range(first_occurrence, len(transmissions)):
+        if transmissions[i] != target_code:
+            break
+        last_occurrence = i
+
+    return (first_occurrence, last_occurrence)
+
+print(find_frequency_positions([5,7,7,8,8,10], 8))
+print(find_frequency_positions([5,7,7,8,8,10], 6))
+print(find_frequency_positions([], 0))
