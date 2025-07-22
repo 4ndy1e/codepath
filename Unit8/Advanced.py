f511767544800251d3c4f02869deddec838b6130
Andy Le <4ndyle@gmail.com>
2025-07-21 22:59:48 -0700

    Solve advanced problem 1 using loop

---
+++ Unit8/Advanced.py
@@ -0,0 +1,47 @@
+"""
+Problem 1
+input: TreeNode (root of the tree)
+output: list (value of each node in the path from the root to the rightmost leaf)
+constraints: 
+    - only return the rightmost nodes 
+edge cases:
+    - empty tree: return []
+    - one node: return root 
+"""
+
+class TreeNode:
+    def __init__(self, value, left=None, right=None):
+        self.val = value
+        self.left = left
+        self.right = right
+
+def right_vine(root):
+    result = []
+    
+    while root: 
+        result.append(root.val)
+        root = root.right
+        
+    return result
+"""
+        Root
+      /      \
+    Node1    Node2
+  /         /    \
+Leaf1    Leaf2  Leaf3
+"""
+ivy1 = TreeNode("Root", 
+                TreeNode("Node1", TreeNode("Leaf1")),
+                TreeNode("Node2", TreeNode("Leaf2"), TreeNode("Leaf3")))
+
+"""
+      Root
+      /  
+    Node1
+    /
+  Leaf1  
+"""
+ivy2 = TreeNode("Root", TreeNode("Node1", TreeNode("Leaf1")))
+
+print(right_vine(ivy1))
+print(right_vine(ivy2))
