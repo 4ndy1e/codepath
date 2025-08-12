be19c6b6083c5f54b3b8b6b68a0df81d0c503fc0
Andy Le <4ndyle@gmail.com>
2025-08-11 18:48:04 -0700

    Solve standard problem 2 using a loop to iterate through adjacency maxtrix

--- unit10/standard.py
+++ unit10/standard.py
@@ -13,3 +13,29 @@ flights = {
 print(list(flights.keys()))
 print(list(flights.values()))
 print(flights["JFK"])
+
+"""
+Problem 2: There and back
+Input: list (adjecency list representing a graph where nodes[i] = [nodes that node i is connected to])
+Output: boolean (true if all fights have a returning flight)
+Constraints:
+    - all flights must have a connected fligt back to it's current node
+Edge Cases:
+    - empty flights -> True
+"""
+
+def bidirectional_flights(flights):
+    for i in range(len(flights)):
+        connectedFlights = flights[i]
+        
+        for currentFlightNum in connectedFlights:
+            if i not in flights[currentFlightNum]:
+                return False
+            
+    return True
+
+flights1 = [[1, 2], [0], [0, 3], [2]]
+flights2 = [[1, 2], [], [0], [2]]
+
+print(bidirectional_flights(flights1))
+print(bidirectional_flights(flights2))
